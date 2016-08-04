3865dc0fea0e056eb5d073412a60185f0fa3dbca
Andrzej Pankowski <andrzej.piotr.pankowski@gmail.com>
2016-08-04 10:12:04 +0200

    initial dropwizard-based application stub based on a dropwizard maven artifact

---
+++ src/main/java/com/example/MoviesApplication.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class MoviesApplication extends Application<MoviesConfiguration> {
+    private static final Logger log = getLogger(MoviesApplication.class);
+
+
+    public static void main(final String[] args) throws Exception {
+        new MoviesApplication().run(args);
+    }
+
+
+    @Override
+    public String getName() {
+        return "Movies";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<MoviesConfiguration> bootstrap) {
+    }
+
+    @Override
+    public void run(
+            final MoviesConfiguration configuration,
+            final Environment environment) {
+    }
+}
---
+++ src/main/java/com/example/MoviesConfiguration.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class MoviesConfiguration extends Configuration {
+}
